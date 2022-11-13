edf69d59d891774f81b1fa99fd96507b64870a43
Pavel <u_pasha@icloud.com>
2022-11-13 20:56:50 +0300

    Updates

---
+++ precache-manifest.7b8099e2ce0b8c4539a3f1143c4e9fa5.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = [
+  {
+    "revision": "741ec3458374b1545371",
+    "url": "/nursary_primary_school/static/css/main.6d97f889.chunk.css"
+  },
+  {
+    "revision": "741ec3458374b1545371",
+    "url": "/nursary_primary_school/static/js/main.741ec345.chunk.js"
+  },
+  {
+    "revision": "96c36d21bf0c8f3e4c60",
+    "url": "/nursary_primary_school/static/css/1.f23dbe25.chunk.css"
+  },
+  {
+    "revision": "96c36d21bf0c8f3e4c60",
+    "url": "/nursary_primary_school/static/js/1.96c36d21.chunk.js"
+  },
+  {
+    "revision": "5b6943f7979b0a8ef918",
+    "url": "/nursary_primary_school/static/js/runtime~main.5b6943f7.js"
+  },
+  {
+    "revision": "d7be097fe1113f2b2510631fe546db7c",
+    "url": "/nursary_primary_school/static/media/map_preloder.d7be097f.png"
+  },
+  {
+    "revision": "1fb0cc3773b4c17ffda4f5810b1301c7",
+    "url": "/nursary_primary_school/static/media/kid_do_homework.1fb0cc37.png"
+  },
+  {
+    "revision": "50373db809d8fd44ae0ec1760808fda7",
+    "url": "/nursary_primary_school/static/media/kid_smiling.50373db8.png"
+  },
+  {
+    "revision": "505cc7df03cca9bb4dac5b5f457209e2",
+    "url": "/nursary_primary_school/static/media/event1_img1.505cc7df.jpg"
+  },
+  {
+    "revision": "244c18d3669c9cfabdb92b8a8bbe231f",
+    "url": "/nursary_primary_school/static/media/event1_img2.244c18d3.jpg"
+  },
+  {
+    "revision": "7658696aceb131c620d5ece5b51daeed",
+    "url": "/nursary_primary_school/static/media/event1_img3.7658696a.jpg"
+  },
+  {
+    "revision": "029e1b8313fa9074ac329a793cd4dcca",
+    "url": "/nursary_primary_school/static/media/event1_img4.029e1b83.jpg"
+  },
+  {
+    "revision": "304101f8b0e9907e3e5a495a7efa4eae",
+    "url": "/nursary_primary_school/static/media/event2_img1.304101f8.jpg"
+  },
+  {
+    "revision": "4f8611065f6e918d72424cd0afb408db",
+    "url": "/nursary_primary_school/static/media/event2_img2.4f861106.jpg"
+  },
+  {
+    "revision": "8a4d526456b0efef498639cbfa30a4aa",
+    "url": "/nursary_primary_school/static/media/event2_img3.8a4d5264.jpg"
+  },
+  {
+    "revision": "6f1df248859a13210cd8d121d68be022",
+    "url": "/nursary_primary_school/static/media/event2_img4.6f1df248.jpg"
+  },
+  {
+    "revision": "70ce842bc9d1a9c05b50279f06a95efd",
+    "url": "/nursary_primary_school/static/media/home_review_Oleg_Katya.70ce842b.jpg"
+  },
+  {
+    "revision": "85a6ab201e4acdfe8e19d43fb341535a",
+    "url": "/nursary_primary_school/static/media/home_review_Sveta.85a6ab20.jpg"
+  },
+  {
+    "revision": "7182c340900f0e72591d079ae813d8cc",
+    "url": "/nursary_primary_school/static/media/home_review_Olga.7182c340.jpg"
+  },
+  {
+    "revision": "8e5d689b84d9b8af3e2568f695bcd40c",
+    "url": "/nursary_primary_school/static/media/home_review_Andr_Ira.8e5d689b.jpg"
+  },
+  {
+    "revision": "9aaa05dbdec555d3ef9f5375ed39d166",
+    "url": "/nursary_primary_school/static/media/Girl.9aaa05db.png"
+  },
+  {
+    "revision": "005b3e7e3b6c3b8112b5352073cc6047",
+    "url": "/nursary_primary_school/static/media/Boy.005b3e7e.png"
+  },
+  {
+    "revision": "b73abba85f33f171730dff87f5cbee65",
+    "url": "/nursary_primary_school/static/media/GirlBoyBalls.b73abba8.png"
+  },
+  {
+    "revision": "93e12a7c5e1ee01ccc757fe5168772e8",
+    "url": "/nursary_primary_school/static/media/Kids_in_Class.93e12a7c.png"
+  },
+  {
+    "revision": "69221c8593bf7152d8ff9859fa9dbf30",
+    "url": "/nursary_primary_school/static/media/Galina_Petrovna_nursary_teacher.69221c85.jpg"
+  },
+  {
+    "revision": "cf899e0b8e766ca6d39311a9a9539aa2",
+    "url": "/nursary_primary_school/static/media/Hatina_Svetlana_nursary_teacher.cf899e0b.jpg"
+  },
+  {
+    "revision": "42e5353442b7657786c3d693470ba859",
+    "url": "/nursary_primary_school/static/media/Menshova_Irina_nursary_teacher.42e53534.jpg"
+  },
+  {
+    "revision": "248cbad55ab3c61e04c56f8016cdc11d",
+    "url": "/nursary_primary_school/static/media/Stepanova_Olga.248cbad5.jpg"
+  },
+  {
+    "revision": "bf4ccc3f5ab1870fd452da9566a62cf4",
+    "url": "/nursary_primary_school/static/media/Mironova_Irina.bf4ccc3f.jpg"
+  },
+  {
+    "revision": "dd03854ca4265297995bc044c3f12942",
+    "url": "/nursary_primary_school/static/media/Swiridov_Pavel.dd03854c.jpg"
+  },
+  {
+    "revision": "7d9fe9b6f8d790667af545b241dd15e0",
+    "url": "/nursary_primary_school/static/media/Swiridova_Alesya.7d9fe9b6.jpg"
+  },
+  {
+    "revision": "5223775c70a8ee4dbede1bf24a732177",
+    "url": "/nursary_primary_school/static/media/Igorev_Iliya.5223775c.jpg"
+  },
+  {
+    "revision": "5051e520684a23e16d7cd0c018f44a08",
+    "url": "/nursary_primary_school/static/media/Golubena_Irina.5051e520.jpg"
+  },
+  {
+    "revision": "4a88b14809095b5a5a4a4e81ff141cc3",
+    "url": "/nursary_primary_school/static/media/Mediinaya_Olga.4a88b148.jpg"
+  },
+  {
+    "revision": "903e4a055c7a0681979b44e98196a65e",
+    "url": "/nursary_primary_school/static/media/Petrovskaya_Olga.903e4a05.jpg"
+  },
+  {
+    "revision": "798ebd3b645701228fd899cb05b2a1f7",
+    "url": "/nursary_primary_school/static/media/Petrig_Vitalina.798ebd3b.jpg"
+  },
+  {
+    "revision": "e1d4177d1a8f773bf026b5574271234b",
+    "url": "/nursary_primary_school/static/media/Ijevskaya_Tatyana.e1d4177d.jpg"
+  },
+  {
+    "revision": "c5adbfd4a2b3b884698f3e04da08fb61",
+    "url": "/nursary_primary_school/static/media/Pavlovitch_Ekaterina.c5adbfd4.jpg"
+  },
+  {
+    "revision": "5511e3f38fd94c5904972870fec45292",
+    "url": "/nursary_primary_school/static/media/Volskaya_Irina.5511e3f3.jpg"
+  },
+  {
+    "revision": "2f96bfc4a313da01f010eff7eee51d85",
+    "url": "/nursary_primary_school/static/media/Slavnaya_Tatyana.2f96bfc4.jpg"
+  },
+  {
+    "revision": "0406edc062dd15d0ae582dad76a94682",
+    "url": "/nursary_primary_school/static/media/baby_crawl.0406edc0.png"
+  },
+  {
+    "revision": "754e1ceada5c3494df05e3d5df51acfc",
+    "url": "/nursary_primary_school/static/media/kids_creation.754e1cea.png"
+  },
+  {
+    "revision": "c31dd51846e487899bfd215e2bdf840e",
+    "url": "/nursary_primary_school/static/media/kids_selfy.c31dd518.png"
+  },
+  {
+    "revision": "1db070ac35a26b2932a4c4d515bd8b2b",
+    "url": "/nursary_primary_school/static/media/girl_books.1db070ac.png"
+  },
+  {
+    "revision": "56888e5f7e0809dd896a3eb0f59d9289",
+    "url": "/nursary_primary_school/static/media/primary_image_1.56888e5f.png"
+  },
+  {
+    "revision": "a42084b371d66806bfb3c29aeac1da99",
+    "url": "/nursary_primary_school/static/media/primary_image_2.a42084b3.png"
+  },
+  {
+    "revision": "574c2db75ef4af999eeebf9a4fbc49fe",
+    "url": "/nursary_primary_school/static/media/primary_image_3.574c2db7.png"
+  },
+  {
+    "revision": "8be0d2d2c94ccf99ec6c3892a68eb5e9",
+    "url": "/nursary_primary_school/static/media/primary_image_4.8be0d2d2.png"
+  },
+  {
+    "revision": "15c68c0e2a6748f6d964d7c4b76070ce",
+    "url": "/nursary_primary_school/static/media/kids_playing_together.15c68c0e.png"
+  },
+  {
+    "revision": "424fd01760882acb43b6eb14a31dfb5a",
+    "url": "/nursary_primary_school/static/media/Home_Babies.424fd017.png"
+  },
+  {
+    "revision": "022df21c79c0535c4ae0020acb189184",
+    "url": "/nursary_primary_school/static/media/poster_video.022df21c.png"
+  },
+  {
+    "revision": "b836fd1bcb803e9983071a090f38cdef",
+    "url": "/nursary_primary_school/static/media/kids_running.b836fd1b.png"
+  },
+  {
+    "revision": "d5381f83a11e17018ce37b1f00e4c5cb",
+    "url": "/nursary_primary_school/static/media/kids_learning.d5381f83.png"
+  },
+  {
+    "revision": "1ea4d669526880be2cb9f7e5304c2c6c",
+    "url": "/nursary_primary_school/static/media/event_country_Iliinka.1ea4d669.jpg"
+  },
+  {
+    "revision": "c22621d50a36724722b230ee66236d68",
+    "url": "/nursary_primary_school/static/media/event_congratulation_8th_march.c22621d5.jpg"
+  },
+  {
+    "revision": "75d2afd801dcaaf026bd24904021ace0",
+    "url": "/nursary_primary_school/index.html"
+  }
+];
